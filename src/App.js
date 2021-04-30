e5031c10ae52b1e5aed0e7b70aaa084ea1b6d54c
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-04-30 10:58:41 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-
+import React from 'react'
 import './App.css';
+
+const api = {
+  key:'0d08ad399b63fcdcf6ece34e1c2a8c7d',
+  base:'https://api.openweathermap.org/data/2.5/'
+}
 
 function App() {
   return (
