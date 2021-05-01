cd21a7015bf41fbe95b5bd2cfb3a4fec22977f4e
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-04-30 21:42:59 -0400

    update

--- src/App.js
+++ src/App.js
@@ -46,17 +46,17 @@ function App() {
             value={query}
             onKeyPress={search}
           />
+          <div className='title'>Harvey's Weather App</div>
        </div>
        {(typeof weather.main !='undefined') ? (
          <div>
        <div className="location-box">
-            <div className='title'>Harvey's Weather App</div>
             <div className="location">{weather.name}, {weather.sys.country}</div>
             <div className="date">{dateBuilder(new Date())}</div>
         </div>
         <div className='weather-box'>
-          <div className='temp'>{weather.main.temp}</div>
-          <div className='weather'>Rain</div>
+          <div className='temp'>{Math.round(weather.main.temp)}°c</div>
+          <div className='weather'>{weather.weather[0].main}</div>
           </div>
         </div>
        ): ('')}
