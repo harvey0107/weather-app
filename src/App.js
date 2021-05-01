153369ec02cb70a3fb77136e10273be1ae12eb80
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-04-30 21:56:45 -0400

    update

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
      <main>
      <div className='title'>WEATHER APP</div>
        <div className='search-box'>
