e4b9690fb05beffe77fbcbb0c1f4353c1560d579
Harvery <harvery@Harverys-MacBook-Pro.local>
2021-04-30 21:18:59 -0400

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,11 @@ function App() {
     if(input.key==='Enter'){
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
       .then(res => res.json())
-      .then(result => setWeather(result))
+      .then(result => {
+        setWeather(result);
+        setQuery('');
+        console.log(weather);
+      })
     }
   }
 
@@ -38,7 +42,7 @@ function App() {
             type='text'
             className='search-bar'
             placeholder='Search Place...'
-            onChange={event => (event.target.value)}
+            onChange={event => setQuery(event.target.value)}
             value={query}
             onKeyPress={search}
           />
